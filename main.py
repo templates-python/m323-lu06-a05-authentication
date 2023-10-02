7fed1f4cb87db6bc29fcfddc28b7817b75f304b9
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-10-02 15:30:49 +0200

    first try

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from flask import Flask, jsonify, request
+from todoItem import TodoItem  # Stellen Sie sicher, dass die TodoItem-Klasse importiert ist
+from todoDao import TodoDao  # Stellen Sie sicher, dass die TodoDao-Klasse importiert ist
+
+app = Flask(__name__)
+dao = TodoDao('todo_example.db')
+dao.create_table()
+
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.get_json()
+    new_item = TodoItem(None, data['title'], data['is_completed'])
+    dao.add_item(new_item)
+    return jsonify({"message": "Todo item created"}), 201
+
+
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    items = dao.get_all_items()
+    return jsonify([item.__dict__ for item in items]), 200
+
+
+@app.route('/todos/<int:item_id>', methods=['GET'])
+def get_todo(item_id):
+    item = dao.get_item(item_id)
+    if item:
+        return jsonify(item.__dict__), 200
+    else:
+        return jsonify({"message": "Item not found"}), 404
+
+
+@app.route('/todos/<int:item_id>', methods=['PUT'])
+def update_todo(item_id):
+    data = request.get_json()
+    updated_item = TodoItem(item_id, data['title'], data['is_completed'])
+    if dao.update_item(updated_item):
+        return jsonify({"message": "Item updated"}), 200
+    else:
+        return jsonify({"message": "Item not found or not updated"}), 404
+
+
+@app.route('/todos/<int:item_id>', methods=['DELETE'])
+def delete_todo(item_id):
+    if dao.delete_item(item_id):
+        return jsonify({"message": "Item deleted"}), 200
+    else:
+        return jsonify({"message": "Item not found or not deleted"}), 404
 
 
 if __name__ == '__main__':
-    calculate()
+    app.run()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,31 @@
-import main
+import pytest
+from main import app
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_add_todo(client):
+    response = client.post('/todos', json={"title": "Test Todo", "is_completed": False})
+    assert response.status_code == 201
+
+def test_get_all_todos(client):
+    response = client.get('/todos')
+    assert response.status_code == 200
+
+def test_get_specific_todo(client):
+    response = client.get('/todos/1')
+    if response.status_code != 404:
+        assert response.status_code == 200
+
+def test_update_todo(client):
+    response = client.put('/todos/1', json={"title": "Updated Test Todo", "is_completed": True})
+    if response.status_code != 404:
+        assert response.status_code == 200
+
+def test_delete_todo(client):
+    response = client.delete('/todos/1')
+    if response.status_code != 404:
+        assert response.status_code == 200
