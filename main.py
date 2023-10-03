bb80522ee0b4b824d600b1e2ad1e938eb66fa756
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-10-03 13:28:00 +0200

    done with main

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from flask import Flask, jsonify, request
-from todoItem import TodoItem  # Stellen Sie sicher, dass die TodoItem-Klasse importiert ist
-from todoDao import TodoDao  # Stellen Sie sicher, dass die TodoDao-Klasse importiert ist
+from todoItem import TodoItem
+from todoDao import TodoDao
 
 app = Flask(__name__)
-dao = TodoDao('todo_example.db')
-dao.create_table()
+todo_dao = TodoDao('todo_example.db')
 
 
 @app.route('/todos', methods=['POST'])
 def add_todo():
     data = request.get_json()
     new_item = TodoItem(None, data['title'], data['is_completed'])
-    dao.add_item(new_item)
+    todo_dao.add_item(new_item)
     return jsonify({"message": "Todo item created"}), 201
 
 
 @app.route('/todos', methods=['GET'])
 def get_all_todos():
-    items = dao.get_all_items()
+    items = todo_dao.get_all_items()
     return jsonify([item.__dict__ for item in items]), 200
 
 
 @app.route('/todos/<int:item_id>', methods=['GET'])
 def get_todo(item_id):
-    item = dao.get_item(item_id)
+    item = todo_dao.get_item(item_id)
     if item:
         return jsonify(item.__dict__), 200
     else:
@@ -34,7 +33,7 @@ def get_todo(item_id):
 def update_todo(item_id):
     data = request.get_json()
     updated_item = TodoItem(item_id, data['title'], data['is_completed'])
-    if dao.update_item(updated_item):
+    if todo_dao.update_item(updated_item):
         return jsonify({"message": "Item updated"}), 200
     else:
         return jsonify({"message": "Item not found or not updated"}), 404
@@ -42,11 +41,18 @@ def update_todo(item_id):
 
 @app.route('/todos/<int:item_id>', methods=['DELETE'])
 def delete_todo(item_id):
-    if dao.delete_item(item_id):
+    if todo_dao.delete_item(item_id):
         return jsonify({"message": "Item deleted"}), 200
     else:
         return jsonify({"message": "Item not found or not deleted"}), 404
 
 
 if __name__ == '__main__':
+    # Generate todo items
+    todo_dao.create_table()
+    todo_dao.add_item(TodoItem(1, 'Buy milk', False))
+    todo_dao.add_item(TodoItem(2, 'Buy eggs', False))
+    todo_dao.add_item(TodoItem(3, 'Buy bread', False))
+    todo_dao.add_item(TodoItem(4, 'Buy butter', False))
+
     app.run()
