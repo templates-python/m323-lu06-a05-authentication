f0a528c212d76b2e45687b354a2178c08018fa68
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-10-03 11:57:38 +0200

    added solution

--- main.py
+++ main.py
@@ -1,52 +1,97 @@
 from flask import Flask, jsonify, request
-from todoItem import TodoItem  # Stellen Sie sicher, dass die TodoItem-Klasse importiert ist
-from todoDao import TodoDao  # Stellen Sie sicher, dass die TodoDao-Klasse importiert ist
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+
+from todoItem import TodoItem
+from user import User
+from todoDao import TodoDao
+from userDao import UserDao
 
 app = Flask(__name__)
-dao = TodoDao('todo_example.db')
-dao.create_table()
+app.secret_key = 'supersecretkey'
+
+todo_dao = TodoDao('todo_example.db')
+user_dao = UserDao('todo_example.db')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/todos', methods=['POST'])
-def add_todo():
+@login_manager.user_loader
+def load_user(user_id):
+    return user_dao.get_user_by_id(int(user_id))
+
+
+@app.route('/login', methods=['POST'])
+def login():
     data = request.get_json()
-    new_item = TodoItem(None, data['title'], data['is_completed'])
-    dao.add_item(new_item)
-    return jsonify({"message": "Todo item created"}), 201
+    user = user_dao.get_user_by_username(data['username'])
+    if user and user.password == data['password']:
+        login_user(user)
+        return jsonify({'success': True}), 200
+    return jsonify({'error': 'Invalid username or password'}), 401
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'success': True}), 200
 
 
 @app.route('/todos', methods=['GET'])
 def get_all_todos():
-    items = dao.get_all_items()
+    items = todo_dao.get_all_items()
     return jsonify([item.__dict__ for item in items]), 200
 
 
 @app.route('/todos/<int:item_id>', methods=['GET'])
 def get_todo(item_id):
-    item = dao.get_item(item_id)
+    item = todo_dao.get_item(item_id)
     if item:
         return jsonify(item.__dict__), 200
     else:
         return jsonify({"message": "Item not found"}), 404
 
 
+@app.route('/todos', methods=['POST'])
+@login_required
+def add_todo():
+    data = request.get_json()
+    new_item = TodoItem(None, data['title'], data['is_completed'])
+    todo_dao.add_item(new_item)
+    return jsonify({"message": "Todo item created"}), 201
+
+
 @app.route('/todos/<int:item_id>', methods=['PUT'])
+@login_required
 def update_todo(item_id):
     data = request.get_json()
     updated_item = TodoItem(item_id, data['title'], data['is_completed'])
-    if dao.update_item(updated_item):
+    if todo_dao.update_item(updated_item):
         return jsonify({"message": "Item updated"}), 200
     else:
         return jsonify({"message": "Item not found or not updated"}), 404
 
 
 @app.route('/todos/<int:item_id>', methods=['DELETE'])
+@login_required
 def delete_todo(item_id):
-    if dao.delete_item(item_id):
+    if todo_dao.delete_item(item_id):
         return jsonify({"message": "Item deleted"}), 200
     else:
         return jsonify({"message": "Item not found or not deleted"}), 404
 
 
 if __name__ == '__main__':
-    app.run()
+    # Generate user
+    user_dao.create_user_table()
+    user_dao.add_user(User(1, 'admin', 'admin@example', 'admin'))
+
+    # Generate todo items
+    todo_dao.create_table()
+    todo_dao.add_item(TodoItem(1, 'Buy milk', False))
+    todo_dao.add_item(TodoItem(2, 'Buy eggs', False))
+    todo_dao.add_item(TodoItem(3, 'Buy bread', False))
+    todo_dao.add_item(TodoItem(4, 'Buy butter', False))
+
+    app.run(debug=True)
