2a43669ee2ca44c09d8b977bb5057c40f66540b8
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:07:50 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def add_todo():
     data = request.get_json()
     new_item = TodoItem(None, data['title'], data['is_completed'])
     todo_dao.add_item(new_item)
-    return jsonify({"message": "Todo item created"}), 201
+    return jsonify({'message': 'Todo item created'}), 201
 
 
 @app.route('/todos', methods=['GET'])
@@ -26,7 +26,7 @@ def get_todo(item_id):
     if item:
         return jsonify(item.__dict__), 200
     else:
-        return jsonify({"message": "Item not found"}), 404
+        return jsonify({'message': 'Item not found'}), 404
 
 
 @app.route('/todos/<int:item_id>', methods=['PUT'])
@@ -34,17 +34,17 @@ def update_todo(item_id):
     data = request.get_json()
     updated_item = TodoItem(item_id, data['title'], data['is_completed'])
     if todo_dao.update_item(updated_item):
-        return jsonify({"message": "Item updated"}), 200
+        return jsonify({'message': 'Item updated'}), 200
     else:
-        return jsonify({"message": "Item not found or not updated"}), 404
+        return jsonify({'message': 'Item not found or not updated'}), 404
 
 
 @app.route('/todos/<int:item_id>', methods=['DELETE'])
 def delete_todo(item_id):
     if todo_dao.delete_item(item_id):
-        return jsonify({"message": "Item deleted"}), 200
+        return jsonify({'message': 'Item deleted'}), 200
     else:
-        return jsonify({"message": "Item not found or not deleted"}), 404
+        return jsonify({'message': 'Item not found or not deleted'}), 404
 
 
 def generate_testdata():
